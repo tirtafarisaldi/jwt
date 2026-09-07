523319bdcbe5fd34eb02ece219649b5dc25f087a
tirtafarisaldi <tirtafarisaldi@gmail.com>
2026-09-07 21:26:17 +0700

    fix postgres dialect for vercel

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,12 @@
 import "./LoadEnv.js";
 import {Sequelize} from "sequelize";
+import pg from "pg";
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 5432,
     dialect: process.env.DB_DIALECT || "postgres",
+    dialectModule: pg,
     logging: false,
     dialectOptions: process.env.DB_HOST !== "localhost" ? {
         ssl: {
